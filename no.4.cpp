52ad298f5c992a7d80cdf3c729eee58c06db19c1
andidhetryansah <49136561+andidhetryansah@users.noreply.github.com>
2019-04-02 13:22:23 +0800

    Create no.4.cpp

---
+++ no.4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int bil;
+  cout<<"masukkan nilai :";
+  cin>>bil;
+  if (bil==100){
+    cout<<"nilai" <<bil<<" = A ";
+  }else if (bil==80){
+  cout<<"nilai" <<bil<<" = A ";
+  }else if (bil==70){
+  cout<<"nilai" <<bil<<" = B ";
+  }else if (bil==60){
+  cout<<"nilai" <<bil<<" = C ";
+  }else if (bil==0){
+  cout<<"nilai" <<bil<<" = E ";
+  }
+ return 0;
+}
